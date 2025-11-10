bbfb4b2bcd7a13ad7aa20b8683e827127d3b5ed0
TTManh <manhtthe187058@fpt.edu.vn>
2025-11-10 22:01:53 +0700

    update proj

---
+++ MarathonManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MarathonManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

3c7502b89b1c6f411b3a827bdd7bca3d6f148d26
tungphamac <tungpham.ac@gmail.com>
2025-11-13 14:01:42 +0700

    commit

--- MarathonManager.Web/obj/Debug/net8.0/MarathonManager.Web.AssemblyInfo.cs
+++ MarathonManager.Web/obj/Debug/net8.0/MarathonManager.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarathonManager.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d282314f16f33359d78cf28bad8ea47d54c14652")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cf74822d85a3f0f63acc82caad78eeee02c4f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarathonManager.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarathonManager.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

dba8c670d2c89f78294186c66c67b5ab12cd80c4
TTManh <manhtthe187058@fpt.edu.vn>
2025-11-14 02:34:45 +0700

    Profile

---
+++ MarathonManager.Web/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MarathonManager.Web.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public bool IsActive { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}

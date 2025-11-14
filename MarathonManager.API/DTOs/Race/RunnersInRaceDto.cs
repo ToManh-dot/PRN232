5451637f1b4af19c08753cc0dc480292356d44d0
tungphamac <tungpham.ac@gmail.com>
2025-11-14 10:30:25 +0700

    add-list-runner-for-race

---
+++ MarathonManager.API/DTOs/Race/RunnersInRaceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MarathonManager.API.DTOs.Race
+{
+    public class RunnersInRaceDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
---
+++ MarathonManager.Web/DTOs/Race/RunnersInRaceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MarathonManager.Web.DTOs.Race
+{
+    public class RunnersInRaceDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}

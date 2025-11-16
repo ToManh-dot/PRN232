a0c40e3d23e553d403507c63725b8b5dd5894311
minhpn2004 <tp029223@gmail.com>
2025-11-16 21:19:05 +0700

    commit

--- MarathonManager.Web/obj/Debug/net8.0/MarathonManager.Web.AssemblyInfo.cs
+++ MarathonManager.Web/obj/Debug/net8.0/MarathonManager.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarathonManager.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f301bc00552a1d5ccb1442b75a4dfa64498a1c9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd15446efc1a6028b6953334bb7ea3c9a6bca72")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarathonManager.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarathonManager.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
